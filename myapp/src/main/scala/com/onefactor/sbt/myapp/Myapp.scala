1d666b2631dc0c462473e2c1c23ad79de18cb79f
nikolay.toroptsev <nikolay.toroptsev@onefactor.com>
2019-01-11 12:23:45 +0300

    increment version

--- myapp/src/main/scala/com/onefactor/sbt/myapp/Myapp.scala
+++ myapp/src/main/scala/com/onefactor/sbt/myapp/Myapp.scala
@@ -1,14 +1,6 @@
 package com.onefactor.sbt.myapp
 
-import com.onefactor.sbt.module1
-import com.onefactor.sbt.module2
-
 
 object Myapp extends App {
-      val version = Version("1.3.1-SNAPSHOT").get
-      println(version)
-      val release = version.copy(qualifier = None)
-      println(release.string)
-      val bumpedMinor = version.bumpMinor
-      println(bumpedMinor.string)
+
 }
